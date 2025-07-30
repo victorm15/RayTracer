2687e92469d1fa9aa397874f4f0a33caea8c3d49
victorm15 <vmauger449@gmail.com>
2025-07-30 15:44:36 +0200

    init commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    auto lang = "C++";
+    std::cout << "Hello and welcome to " << lang << "!\n";
+    std::cout << "Hello\n";
+
+    for (int i = 1; i <= 5; i++) {
+
+        std::cout << "i = " << i << "\n";
+    }
+
+
+    return 0;
+
+}
